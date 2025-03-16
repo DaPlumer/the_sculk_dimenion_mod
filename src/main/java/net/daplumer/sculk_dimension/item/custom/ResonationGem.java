6926e599fb68a3897d8c3dd4ba57819c6d52037c
DaPlumer <daplumer271@gmail.com>
2025-03-16 11:38:55 -0400

    added a vibration particle emitter to the resonation gem

--- src/main/java/net/daplumer/sculk_dimension/item/custom/ResonationGem.java
+++ src/main/java/net/daplumer/sculk_dimension/item/custom/ResonationGem.java
@@ -6,17 +6,23 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particle.VibrationParticleEffect;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
-import net.minecraft.world.event.GameEvent;
+import net.minecraft.world.event.*;
+import org.jetbrains.annotations.Nullable;
+import static net.daplumer.sculk_dimension.util.VecToBlockPos.createBlockPos;
 
 public class ResonationGem extends Item{
+    private int tickSinceLastUse = 0;
+    private HitResult lastRay;
+    private Vec3d rayHit;
 
     public ResonationGem(Settings settings) {
         super(settings);
@@ -33,22 +39,25 @@ public class ResonationGem extends Item{
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         Entity entity;
         ray = raycastEntity(user, 256);
-        user.getItemCooldownManager().set(this,32);
+        this.lastRay = ray;
+        this.tickSinceLastUse = 0;
         if (ray == null){
             return TypedActionResult.fail(user.getStackInHand(hand));
         }
+        this.rayHit = ray.getPos();
         if (ray.getType() == HitResult.Type.ENTITY){
             EntityHitResult entityHit = (EntityHitResult) ray;
             entity = entityHit.getEntity();
-            world.emitGameEvent(entity, GameEvent.RESONATE_6, entity.getPos());
             user.getStackInHand(hand).damage(1, user, LivingEntity.getSlotForHand(hand));
             entity.playSound(SoundEvents.BLOCK_SCULK_SHRIEKER_FALL,1,1);
             Insanity.increment(user);
-            return TypedActionResult.pass(user.getStackInHand(hand));
+            world.emitGameEvent(((EntityHitResult) this.lastRay).getEntity(), GameEvent.RESONATE_6, ((EntityHitResult) this.lastRay).getEntity().getPos());
+            user.getItemCooldownManager().set(this,32);
+            return TypedActionResult.success(user.getStackInHand(hand), true);
         }
         return super.use(world, user, hand);
     }
-    public static HitResult raycastEntity(PlayerEntity player, double maxDistance) {
+    public static @Nullable HitResult raycastEntity(PlayerEntity player, double maxDistance) {
         Entity cameraEntity = MinecraftClient.getInstance().cameraEntity;
         if (cameraEntity != null) {
             Vec3d cameraPos = player.getCameraPosVec(1.0f);
@@ -58,5 +67,33 @@ public class ResonationGem extends Item{
             return ProjectileUtil.raycast(cameraEntity, cameraPos, rayCastContext, box, (entity -> !entity.isSpectator() && entity.canHit()), maxDistance);
         }
         return null;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        this.tickSinceLastUse++;
+        if (this.lastRay != null){
+            if (this.lastRay.getType() == HitResult.Type.ENTITY){
+                this.rayHit = ((EntityHitResult) this.lastRay).getEntity().getPos();
+            }
+
+            if (world instanceof ServerWorld) {
+                Vec3d vec3d = entity.getPos();
+                PositionSource positionSource = new BlockPositionSource(createBlockPos(this.rayHit));
+                Vec3d vec3d2 = positionSource.getPos(world).orElse(vec3d);
+                int i = this.tickSinceLastUse;
+                int j = MathHelper.floor(entity.getPos().distanceTo(this.rayHit));
+                double d = 1.0 - (double)i / (double)j;
+                if(d > 0) {
+                    double e = MathHelper.lerp(d, vec3d.x, vec3d2.x);
+                    double f = MathHelper.lerp(d, vec3d.y, vec3d2.y);
+                    double g = MathHelper.lerp(d, vec3d.z, vec3d2.z);
+                    ((ServerWorld) world).spawnParticles(new VibrationParticleEffect(positionSource, i), e, f, g, 1, 0.0, 0.0, 0.0, 0.0);
+                    if (d - (1D / (double) j) <= 0 && this.lastRay.getType() == HitResult.Type.ENTITY){
+                        world.emitGameEvent(((EntityHitResult) this.lastRay).getEntity(), GameEvent.RESONATE_6, ((EntityHitResult) this.lastRay).getEntity().getPos());
+                    }
+                }
+            }
+        }
     }
 }
