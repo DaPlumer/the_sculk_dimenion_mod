1956472f64505339cfcbfc39077ce03c9d5b18bb
DaPlumer <daplumer271@gmail.com>
2025-05-03 15:20:08 -0400

    refactor item registration for improved readability

--- src/main/java/net/daplumer/sculk_dimension/item/ModItems.java
+++ src/main/java/net/daplumer/sculk_dimension/item/ModItems.java
@@ -7,6 +7,8 @@ import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
@@ -21,16 +23,58 @@ public class ModItems {
     public static final Item SCULK_BRICK =ITEMS.register("sculk_brick", new Item.Settings().rarity(Rarity.UNCOMMON));
     public static final Item SCULK_BRICK_CASING =ITEMS.register("sculk_brick_casing", new Item.Settings());
 
-    public static final Item CRYSTALIZED_SOUL =ITEMS.register("crystalized_soul", new Item.Settings().rarity(Rarity.UNCOMMON));
-    public static final Item MOSSY_BOOTS = ITEMS.register("mossy_boots", new Item.Settings().armor(ModArmorMaterials.MOSSY_ARMOR, EquipmentType.BOOTS));
-    public static final Item RESOANATION_GEM_STAFF = ITEMS.register("resonation_gem_staff", new Item.Settings().maxCount(1).rarity(Rarity.UNCOMMON));
-    public static final Item RESOANATION_GEM = ITEMS.register("resonation_gem", new  Item.Settings().maxDamage(25).maxCount(1).rarity(Rarity.UNCOMMON), ResonationGem::new);
-    public static final Item BROKEN_ECHO = ITEMS.register("broken_echo", new Item.Settings().maxCount(1).rarity(Rarity.RARE), BrokenEcho::new);
-    public static final Item MOSSY_BAG = ITEMS.register("mossy_bag", new  Item.Settings().maxCount(16), MossyBag::new);
-    public static final Item RESONANT_POLLEN = ITEMS.register("resonant_pollen", new Item.Settings().maxCount(16));
-    public static final Item WAX_BRICK = ITEMS.register("wax_brick", new Item.Settings().maxCount(16).useRemainder(MOSSY_BAG), WaxBrick::new);
-    public static final Item ECHO_MEDALLION = ITEMS.register("echo_totem",new Item.Settings().maxCount(1).rarity(Rarity.UNCOMMON));
-    public static final Item MEMORY_GEM = ITEMS.register("memory_gem",new Item.Settings().maxCount(1).maxDamage(256).rarity(Rarity.UNCOMMON), MemoryGemKT::new);
+    public static final Item CRYSTALIZED_SOUL =ITEMS.register("crystalized_soul",
+            new Item.Settings()
+                    .rarity(Rarity.UNCOMMON)
+    );
+    public static final Item MOSSY_BOOTS = ITEMS.register("mossy_boots",
+            new Item.Settings()
+                    .armor(ModArmorMaterials.MOSSY_ARMOR, EquipmentType.BOOTS)
+    );
+    public static final Item RESOANATION_GEM_STAFF = ITEMS.register("resonation_gem_staff",
+            new Item.Settings()
+                    .maxCount(1)
+                    .rarity(Rarity.UNCOMMON)
+    );
+    public static final Item RESOANATION_GEM = ITEMS.register("resonation_gem",
+            new Item.Settings()
+                    .maxDamage(25)
+                    .maxCount(1)
+                    .rarity(Rarity.UNCOMMON),
+            ResonationGem::new
+    );
+    public static final Item BROKEN_ECHO = ITEMS.register("broken_echo",
+            new Item.Settings()
+                    .maxCount(1)
+                    .rarity(Rarity.RARE),
+            BrokenEcho::new
+    );
+    public static final Item MOSSY_BAG = ITEMS.register("mossy_bag",
+            new Item.Settings()
+                    .maxCount(16),
+            MossyBag::new
+    );
+    public static final Item RESONANT_POLLEN = ITEMS.register("resonant_pollen",
+            new Item.Settings()
+                    .maxCount(16)
+    );
+    public static final Item WAX_BRICK = ITEMS.register("wax_brick",
+            new Item.Settings()
+                    .maxCount(16)
+                    .useRemainder(MOSSY_BAG),
+            WaxBrick::new
+    );
+    public static final Item ECHO_MEDALLION = ITEMS.register("echo_totem",
+            new Item.Settings()
+                    .maxCount(1)
+                    .rarity(Rarity.UNCOMMON)
+    );
+    public static final Item MEMORY_GEM = ITEMS.register("memory_gem",
+            new Item.Settings()
+                    .maxCount(1)
+                    .maxDamage(256)
+                    .rarity(Rarity.UNCOMMON),
+            MemoryGemKT::new);
 
     public static void registerModItems(){
         TheSculkDimension.LOGGER.info("Registering Mod Items for " + TheSculkDimension.MOD_ID );
