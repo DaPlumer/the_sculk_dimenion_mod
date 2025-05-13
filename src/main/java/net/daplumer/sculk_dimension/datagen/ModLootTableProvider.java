30efbeb52d8ef2b69f9812b56ff0b4857c28ba18
DaPlumer <daplumer271@gmail.com>
2025-05-13 15:17:20 -0400

    add echoing bloom to loot table

--- src/main/java/net/daplumer/sculk_dimension/datagen/ModLootTableProvider.java
+++ src/main/java/net/daplumer/sculk_dimension/datagen/ModLootTableProvider.java
@@ -5,6 +5,7 @@ import net.daplumer.sculk_dimension.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.minecraft.registry.RegistryWrapper;
+
 import java.util.concurrent.CompletableFuture;
 
 public class ModLootTableProvider extends FabricBlockLootTableProvider {
@@ -16,5 +17,6 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
         addDrop(ModBlocks.MEMORY_GEM_ORE, oreDrops(ModBlocks.MEMORY_GEM_ORE, ModItems.MEMORY_GEM));
         addDrop(ModBlocks.MEMORY_GEM_DEEPSLATE_ORE, oreDrops(ModBlocks.MEMORY_GEM_DEEPSLATE_ORE, ModItems.MEMORY_GEM));
         addDrop(ModBlocks.ENCHANTMENT_DUPLICATOR);
+        addVinePlantDrop(ModBlocks.ECHOING_BLOOM,ModBlocks.ECHOING_BLOOM_TIP);
     }
 }
